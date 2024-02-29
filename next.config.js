2ae71be05d74e4f1c2d7f625585ff7c1b848dfb0
aly25 <aly25@wgu.edu>
2024-02-29 09:57:42 -0500

    CONFIG

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-const path = require('path')
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
  
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  }
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
+ 
+module.exports = nextConfig
