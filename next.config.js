b214787e73b6a165f40d597c998c1a256c3808f7
aly25 <aly25@wgu.edu>
2024-02-29 09:30:40 -0500

    Reverted config JS.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-/** @type {import('next').NextConfig} */
 const path = require('path')
-const NextConfig = {
-  output: 'export',
-}
  
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  },
-  NextConfig
+  }
 }
