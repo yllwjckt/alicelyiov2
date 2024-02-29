3165a7102cfcb68e052f63f0d721b8a58e6d573f
aly25 <aly25@wgu.edu>
2024-02-29 09:29:54 -0500

    Added export to config.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+/** @type {import('next').NextConfig} */
 const path = require('path')
+const NextConfig = {
+  output: 'export',
+}
  
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  }
+  },
+  NextConfig
 }
